7b77685454b695a027d80c4dd7a2942fa9b26e0c
Areg Vardanian <aregv@bk.ru>
2021-12-04 17:23:34 +0300

    Setup clean swift

---
+++ VKNewsFeed/Newsfeed/NewsfeedModels.swift
@@ -0,0 +1,31 @@
+//
+//  NewsfeedModels.swift
+//  VKNewsFeed
+//
+//  Created by Геворг on 04.12.2021.
+//  Copyright (c) 2021 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+enum Newsfeed {
+   
+  enum Model {
+    struct Request {
+      enum RequestType {
+        case some
+      }
+    }
+    struct Response {
+      enum ResponseType {
+        case some
+      }
+    }
+    struct ViewModel {
+      enum ViewModelData {
+        case some
+      }
+    }
+  }
+  
+}
---
+++ VKNewsFeed/Newsfeed/NewsfeedWorker.swift
@@ -0,0 +1,13 @@
+//
+//  NewsfeedWorker.swift
+//  VKNewsFeed
+//
+//  Created by Геворг on 04.12.2021.
+//  Copyright (c) 2021 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class NewsfeedService {
+
+}
