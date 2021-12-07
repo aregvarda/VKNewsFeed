ae5c706734979bade1331727ecfdb1b1fdcd7667
Areg Vardanian <aregv@bk.ru>
2021-12-07 18:31:38 +0300

    Implementing Post Images JSON Fetching

--- VKNewsFeed/Helpers/WebImageView.swift
+++ VKNewsFeed/Helpers/WebImageView.swift
@@ -9,16 +9,18 @@ import UIKit
 
 class WebImageView: UIImageView {
     
-    func set(imageURL: String) {
-        guard let url = URL(string: imageURL) else { return }
+    func set(imageURL: String?) {
+        guard let imageURL = imageURL, let url = URL(string: imageURL) else { return }
         
         if let cachedResponse = URLCache.shared.cachedResponse(for: URLRequest(url: url)) {
             self.image = UIImage(data: cachedResponse.data)
-            print("from cache")
+            //print("from cachе")
             return
         }
-        print("from internet")
-        let dataTask = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+        
+        //print("from internet")
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
             
             DispatchQueue.main.async {
                 if let data = data, let response = response {
@@ -29,9 +31,11 @@ class WebImageView: UIImageView {
         }
         dataTask.resume()
     }
+    
     private func handleLoadedImage(data: Data, response: URLResponse) {
         guard let responseURL = response.url else { return }
         let cachedResponse = CachedURLResponse(response: response, data: data)
         URLCache.shared.storeCachedResponse(cachedResponse, for: URLRequest(url: responseURL))
+        
     }
 }
--- VKNewsFeed/Newsfeed/NewsfeedPresenter.swift
+++ VKNewsFeed/Newsfeed/NewsfeedPresenter.swift
@@ -36,6 +36,7 @@ class NewsfeedPresenter: NewsfeedPresentationLogic {
     private func cellViewModel(from feedItem: FeedItem, profiles: [Profile], groups: [Group]) -> FeedViewModel.Cell {
         
         let profile = self.profile(for: feedItem.sourceId, profiles: profiles, groups: groups)
+        let photoAttachment = self.photoAttachment(feedItem: feedItem)
         let date = Date(timeIntervalSince1970: feedItem.date)
         let dateTitle = dateFormatter.string(from: date)
         
@@ -46,7 +47,8 @@ class NewsfeedPresenter: NewsfeedPresentationLogic {
                                        likes: String(feedItem.likes?.count ?? 0),
                                        comments: String(feedItem.comments?.count ?? 0),
                                        shares: String(feedItem.reposts?.count ?? 0),
-                                       views: String(feedItem.views?.count ?? 0))
+                                       views: String(feedItem.views?.count ?? 0),
+                                       photoAttachment: photoAttachment)
     }
     private func profile(for sourceId: Int, profiles: [Profile], groups: [Group]) -> ProfileRepresentable {
         let profilesOrGroups: [ProfileRepresentable] = sourceId >= 0 ? profiles : groups
@@ -55,5 +57,16 @@ class NewsfeedPresenter: NewsfeedPresentationLogic {
             myProfileRepresentable.id == normalSourceId
         }
         return profileRepresentable!
+    }
+    
+    private func photoAttachment(feedItem: FeedItem) -> FeedViewModel.FeedCellPhotoAttachment? {
+        guard let photos = feedItem.attachments?.compactMap({ attachment in
+            attachment.photo
+        }), let firstPhoto = photos.first else {
+            return nil
+        }
+        return FeedViewModel.FeedCellPhotoAttachment.init(photoUrlString: firstPhoto.srcBIG,
+                                                          width: firstPhoto.width,
+                                                          height: firstPhoto.height)
     }
 }
