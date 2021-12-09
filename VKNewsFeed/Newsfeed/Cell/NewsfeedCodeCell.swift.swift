e2e297fd76546d6124084682c91399ece3db2503
Areg Vardanian <aregv@bk.ru>
2021-12-09 18:46:06 +0300

    Auto Layout Programmatically finish

--- VKNewsFeed/Newsfeed/Cell/NewsfeedCodeCell.swift.swift
+++ VKNewsFeed/Newsfeed/Cell/NewsfeedCodeCell.swift.swift
@@ -15,7 +15,7 @@ final class NewsfeedCodeCell: UITableViewCell {
     // the first layer
     let cardView: UIView = {
         let view = UIView()
-        view.backgroundColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
+        view.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
@@ -23,14 +23,12 @@ final class NewsfeedCodeCell: UITableViewCell {
     let topView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
         return view
     }()
     
     let postLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
-        label.backgroundColor = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
         label.font = Constants.postLabelFont
         label.textColor = #colorLiteral(red: 0.1725490196, green: 0.1764705882, blue: 0.1803921569, alpha: 1)
         return label
@@ -44,8 +42,6 @@ final class NewsfeedCodeCell: UITableViewCell {
     
     let bottomView: UIView = {
         let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
         return view
     }()
     
@@ -54,7 +50,6 @@ final class NewsfeedCodeCell: UITableViewCell {
     let iconImageView: WebImageView = {
         let imageView = WebImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         return imageView
     }()
     
@@ -64,7 +59,6 @@ final class NewsfeedCodeCell: UITableViewCell {
         label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
         label.numberOfLines = 0
         label.textColor = #colorLiteral(red: 0.1725490196, green: 0.1764705882, blue: 0.1803921569, alpha: 1)
-        label.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         return label
     }()
     
@@ -73,17 +67,115 @@ final class NewsfeedCodeCell: UITableViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textColor = #colorLiteral(red: 0.5058823529, green: 0.5490196078, blue: 0.6, alpha: 1)
         label.font = UIFont.systemFont(ofSize: 12)
-        label.backgroundColor = #colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1)
+        return label
+    }()
+    
+    // the third layer (bottomView)
+    
+    let likesView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let commentsView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let sharesView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let viewsView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    // the fourth layer (bottomView)
+    
+    let likesImage: UIImageView = {
+       let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "like")
+        return imageView
+    }()
+    
+    let commentsImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "comment")
+        return imageView
+    }()
+    
+    let sharesImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "share")
+        return imageView
+    }()
+    
+    let viewsImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "eye")
+        return imageView
+    }()
+    
+    let likesLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = #colorLiteral(red: 0.5768421292, green: 0.6187390685, blue: 0.664434731, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.lineBreakMode = .byClipping
+        return label
+    }()
+    
+    let commentsLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = #colorLiteral(red: 0.5768421292, green: 0.6187390685, blue: 0.664434731, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.lineBreakMode = .byClipping
+        return label
+    }()
+    
+    let sharesLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = #colorLiteral(red: 0.5768421292, green: 0.6187390685, blue: 0.664434731, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.lineBreakMode = .byClipping
+        return label
+    }()
+    
+    let viewsLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = #colorLiteral(red: 0.5768421292, green: 0.6187390685, blue: 0.664434731, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.lineBreakMode = .byClipping
         return label
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        backgroundColor = #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)
+        backgroundColor = .clear
+        selectionStyle = .none
+        
+        cardView.layer.cornerRadius = 10
+        cardView.clipsToBounds = true
+        
         overlayFirstLayer() // the first layer
         overlaySecondLayer() // the second layer
         overlayThirdLayerOnTopView() // the third layer (topView)
+        overlayThirdLayerOnBottomView() // the third layer (bottomView)
+        overlayFourthLayerOnBottomViewViews() // the fourth layer (bottomViewViews)
     }
     
     func set(viewModel: FeedCellViewModel) {
@@ -92,6 +184,10 @@ final class NewsfeedCodeCell: UITableViewCell {
         nameLabel.text = viewModel.name
         dateLabel.text = viewModel.date
         postLabel.text = viewModel.text
+        likesLabel.text = viewModel.likes
+        commentsLabel.text = viewModel.comments
+        sharesLabel.text = viewModel.shares
+        viewsLabel.text = viewModel.views
         
         postLabel.frame = viewModel.sizes.postLabelFrame
         postImageView.frame = viewModel.sizes.attachmentFrame
@@ -105,7 +201,75 @@ final class NewsfeedCodeCell: UITableViewCell {
         }
     }
     
-    func overlayThirdLayerOnTopView() {
+    private func overlayFourthLayerOnBottomViewViews() {
+        likesView.addSubview(likesImage)
+        likesView.addSubview(likesLabel)
+        
+        commentsView.addSubview(commentsImage)
+        commentsView.addSubview(commentsLabel)
+        
+        sharesView.addSubview(sharesImage)
+        sharesView.addSubview(sharesLabel)
+        
+        viewsView.addSubview(viewsImage)
+        viewsView.addSubview(viewsLabel)
+        
+        helpInFourthLayer(view: likesView, imageView: likesImage, label: likesLabel)
+        helpInFourthLayer(view: commentsView, imageView: commentsImage, label: commentsLabel)
+        helpInFourthLayer(view: sharesView, imageView: sharesImage, label: sharesLabel)
+        helpInFourthLayer(view: viewsView, imageView: viewsImage, label: viewsLabel)
+    }
+    
+    private func helpInFourthLayer(view: UIView, imageView: UIImageView, label: UILabel) {
+        
+        // imageView constraints
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: Constants.bottomViewViewsIconSize).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: Constants.bottomViewViewsIconSize).isActive = true
+        
+        // label constraints
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 4).isActive = true
+        label.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
+    private func overlayThirdLayerOnBottomView() {
+        bottomView.addSubview(likesView)
+        bottomView.addSubview(commentsView)
+        bottomView.addSubview(sharesView)
+        bottomView.addSubview(viewsView)
+        
+        // likesView constraints
+        likesView.anchor(top: bottomView.topAnchor,
+                         leading: bottomView.leadingAnchor,
+                         bottom: nil,
+                         trailing: nil,
+                         size: CGSize(width: Constants.bottomViewViewWidth, height: Constants.bottomViewViewHeight))
+        
+        // commentsView constraints
+        commentsView.anchor(top: bottomView.topAnchor,
+                         leading: likesView.trailingAnchor,
+                         bottom: nil,
+                         trailing: nil,
+                         size: CGSize(width: Constants.bottomViewViewWidth, height: Constants.bottomViewViewHeight))
+        
+        // sharesView constraints
+        sharesView.anchor(top: bottomView.topAnchor,
+                            leading: commentsView.trailingAnchor,
+                            bottom: nil,
+                            trailing: nil,
+                            size: CGSize(width: Constants.bottomViewViewWidth, height: Constants.bottomViewViewHeight))
+        
+        // viewsView constraints
+        viewsView.anchor(top: bottomView.topAnchor,
+                          leading: nil,
+                          bottom: nil,
+                          trailing: bottomView.trailingAnchor,
+                          size: CGSize(width: Constants.bottomViewViewWidth, height: Constants.bottomViewViewHeight))
+    }
+    
+    private func overlayThirdLayerOnTopView() {
         topView.addSubview(iconImageView)
         topView.addSubview(nameLabel)
         topView.addSubview(dateLabel)
@@ -129,7 +293,7 @@ final class NewsfeedCodeCell: UITableViewCell {
         dateLabel.heightAnchor.constraint(equalToConstant: 14).isActive = true
     }
     
-    func overlaySecondLayer() {
+    private func overlaySecondLayer() {
         cardView.addSubview(topView)
         cardView.addSubview(postLabel)
         cardView.addSubview(postImageView)
@@ -141,14 +305,17 @@ final class NewsfeedCodeCell: UITableViewCell {
         topView.topAnchor.constraint(equalTo: cardView.topAnchor, constant: 8).isActive = true
         topView.heightAnchor.constraint(equalToConstant: Constants.topViewHeight).isActive = true
         
-        // postLabel constraints
+        // postlabel constraints
+        // не нужно, так как размеры задаются динамически
         
         // postImageView constraints
+        // не нужно, так как размеры задаются динамически
         
         // bottomView constraints
+        // не нужно, так как размеры задаются динамически
     }
     
-    func overlayFirstLayer() {
+    private func overlayFirstLayer() {
         addSubview(cardView)
         
         // cardView constraints
