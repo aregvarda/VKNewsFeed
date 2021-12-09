17d33b27b19031ad19ffc74fd45a05589ca66223
Areg Vardanian <aregv@bk.ru>
2021-12-09 16:47:47 +0300

    Auto Layout Programmatically pre finish

--- VKNewsFeed/Newsfeed/Cell/NewsfeedCodeCell.swift.swift
+++ VKNewsFeed/Newsfeed/Cell/NewsfeedCodeCell.swift.swift
@@ -49,25 +49,84 @@ final class NewsfeedCodeCell: UITableViewCell {
         return view
     }()
     
+    // the third layer (topView)
+    
+    let iconImageView: WebImageView = {
+        let imageView = WebImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.numberOfLines = 0
+        label.textColor = #colorLiteral(red: 0.1725490196, green: 0.1764705882, blue: 0.1803921569, alpha: 1)
+        label.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        return label
+    }()
+    
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = #colorLiteral(red: 0.5058823529, green: 0.5490196078, blue: 0.6, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.backgroundColor = #colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1)
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         backgroundColor = #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)
         overlayFirstLayer() // the first layer
         overlaySecondLayer() // the second layer
+        overlayThirdLayerOnTopView() // the third layer (topView)
     }
     
     func set(viewModel: FeedCellViewModel) {
+        
+        iconImageView.set(imageURL: viewModel.iconUrlString)
+        nameLabel.text = viewModel.name
+        dateLabel.text = viewModel.date
+        postLabel.text = viewModel.text
+        
         postLabel.frame = viewModel.sizes.postLabelFrame
         postImageView.frame = viewModel.sizes.attachmentFrame
         bottomView.frame = viewModel.sizes.bottomViewFrame
         
         if let photoAttachment  = viewModel.photoAttachment {
-//            postImageView.set(imageURL: photoAttachment.photoUrlString)
+            postImageView.set(imageURL: photoAttachment.photoUrlString)
             postImageView.isHidden = false
         } else {
             postImageView.isHidden = true
         }
+    }
+    
+    func overlayThirdLayerOnTopView() {
+        topView.addSubview(iconImageView)
+        topView.addSubview(nameLabel)
+        topView.addSubview(dateLabel)
+        
+        // iconImageView constraints
+        iconImageView.leadingAnchor.constraint(equalTo: topView.leadingAnchor).isActive = true
+        iconImageView.topAnchor.constraint(equalTo: topView.topAnchor).isActive = true
+        iconImageView.heightAnchor.constraint(equalToConstant: Constants.topViewHeight).isActive = true
+        iconImageView.widthAnchor.constraint(equalToConstant: Constants.topViewHeight).isActive = true
+        
+        // nameLabel constraints
+        nameLabel.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: 8).isActive = true
+        nameLabel.trailingAnchor.constraint(equalTo: topView.trailingAnchor, constant: -8).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: topView.topAnchor, constant: 2).isActive = true
+        nameLabel.heightAnchor.constraint(equalToConstant: Constants.topViewHeight / 2 - 2).isActive = true
+        
+        // dateLabel constraints
+        dateLabel.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: 8).isActive = true
+        dateLabel.trailingAnchor.constraint(equalTo: topView.trailingAnchor, constant: -8).isActive = true
+        dateLabel.bottomAnchor.constraint(equalTo: topView.bottomAnchor, constant: -2).isActive = true
+        dateLabel.heightAnchor.constraint(equalToConstant: 14).isActive = true
     }
     
     func overlaySecondLayer() {
