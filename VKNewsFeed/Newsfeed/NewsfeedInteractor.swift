9df7700add6dd6ab0b118e598bf9bee263ee7260
Areg Vardanian <aregv@bk.ru>
2021-12-12 18:30:54 +0300

    Previous Posts Footer Loader

--- VKNewsFeed/Newsfeed/NewsfeedInteractor.swift
+++ VKNewsFeed/Newsfeed/NewsfeedInteractor.swift
@@ -9,37 +9,37 @@
 import UIKit
 
 protocol NewsfeedBusinessLogic {
-    func makeRequest(request: Newsfeed.Model.Request.RequestType)
+  func makeRequest(request: Newsfeed.Model.Request.RequestType)
 }
 
 class NewsfeedInteractor: NewsfeedBusinessLogic {
+
+  var presenter: NewsfeedPresentationLogic?
+  var service: NewsfeedService?
     
-    var presenter: NewsfeedPresentationLogic?
-    var service: NewsfeedService?
-    
-    func makeRequest(request: Newsfeed.Model.Request.RequestType) {
-        if service == nil {
-            service = NewsfeedService()
-            
-            switch request {
-            case .getNewsfeed:
-                service?.getFeed(completion: { [weak self] (revealedPostIds, feed) in
-                    self?.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentNewsfeed(feed: feed, revealdedPostIds: revealedPostIds))
-                })
-            case .getUser:
-                service?.getUser(completion: { [weak self] (user) in
-                    self?.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentUserInfo(user: user))
-                })
-            case .revealPostIds(let postId):
-                service?.revealPostIds(forPostId: postId, completion: { [weak self] (revealedPostIds, feed) in
-                    self?.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentNewsfeed(feed: feed, revealdedPostIds: revealedPostIds))
-                })
-            case .getNextBatch:
-                print("123")
-                service?.getNextBatch(completion: { (revealedPostIds, feed) in
-                    self.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentNewsfeed(feed: feed, revealdedPostIds: revealedPostIds))
-                })
-            }
-        }
+  func makeRequest(request: Newsfeed.Model.Request.RequestType) {
+    if service == nil {
+      service = NewsfeedService()
     }
+    
+    switch request {
+    case .getNewsfeed:
+        service?.getFeed(completion: { [weak self] (revealedPostIds, feed) in
+            self?.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentNewsfeed(feed: feed, revealdedPostIds: revealedPostIds))
+        })
+    case .getUser:
+        service?.getUser(completion: { [weak self] (user) in
+            self?.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentUserInfo(user: user))
+        })
+    case .revealPostIds(let postId):
+        service?.revealPostIds(forPostId: postId, completion: { [weak self] (revealedPostIds, feed) in
+            self?.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentNewsfeed(feed: feed, revealdedPostIds: revealedPostIds))
+        })
+    case .getNextBatch:
+        self.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentFooterLoader)
+        service?.getNextBatch(completion: { (revealedPostIds, feed) in
+            self.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentNewsfeed(feed: feed, revealdedPostIds: revealedPostIds))
+        })
+    }
+  }
 }
